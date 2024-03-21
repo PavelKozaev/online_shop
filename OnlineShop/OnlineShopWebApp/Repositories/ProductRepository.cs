00deb40c83bd76b822ec8c5eb3c9ac86aa40851f
PavelKozaev <pkozaev221299@gmail.com>
2024-03-21 22:15:35 +0300

    Исправил название класса.

---
+++ OnlineShop/OnlineShopWebApp/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly List<Product> products = [];
+
+        public ProductRepository()
+        {
+            products.Add(new Product("An Epic Journey to the Center of the Milky Way Galaxy", 1000, ""));
+            products.Add(new Product("Journey to the constellation Centaurus", 500, ""));
+            products.Add(new Product("Journey to Andromeda", 3000, ""));
+        }
+
+        public IEnumerable<Product> GetAllProducts() => products;
+    }
+}
