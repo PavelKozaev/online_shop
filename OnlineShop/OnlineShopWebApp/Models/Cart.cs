c14f0f9daa07075c3a25f948fcd55b08aca7bb83
PavelKozaev <pkozaev221299@gmail.com>
2024-03-28 13:07:43 +0300

    Добавил отображение количества товаров в корзине

--- OnlineShop/OnlineShopWebApp/Models/Cart.cs
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string UserId { get; set; }
         public List<CartItem> Items { get; set;}
-        public decimal Cost => Items.Sum(x => x.Cost);
+        public decimal Cost => Items?.Sum(x => x.Cost) ?? 0;
+        public decimal Amount => Items?.Sum(x => x.Amount) ?? 0;
     }
 }
