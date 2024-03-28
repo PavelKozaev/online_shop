157f657487cf552a625486607f801547d4268de1
PavelKozaev <pkozaev221299@gmail.com>
2024-03-28 15:52:04 +0300

    Revert "Сделал форму авторизации"
    This reverts commit 38e03f6c79c645e7aaf52dbb0b97cd9f4542e60b.

--- OnlineShop/OnlineShopWebApp/Models/Authorization.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineShopWebApp.Models
-{
-    public class Authorization
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-        public bool IsRememberMe { get; set; }
-    }
-}
