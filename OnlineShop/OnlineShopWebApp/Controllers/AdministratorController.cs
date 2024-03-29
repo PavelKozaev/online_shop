9aa69a8cfe052c930f7efb3d7bdab477123f1217
PavelKozaev <pkozaev221299@gmail.com>
2024-03-29 13:15:02 +0300

    Задекларировал модель Administrator и контроллер AdministratorController. Добавил панель администратора с меню

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdministratorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetOrders()
+        {
+            return View();
+        }
+
+        public IActionResult GetUsers()
+        {
+            return View();
+        }
+
+        public IActionResult GetRoles()
+        {
+            return View();
+        }
+
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+    }
+}
