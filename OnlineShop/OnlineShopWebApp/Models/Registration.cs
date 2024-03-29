a12fe43840f24dffb82165508bafa4d1b5a98f15
PavelKozaev <pkozaev221299@gmail.com>
2024-03-29 12:25:02 +0300

    Сделал форму регистрации

---
+++ OnlineShop/OnlineShopWebApp/Models/Registration.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Registration
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+    }
+}
