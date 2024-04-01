cb4e6717a51bf30e4cbee1d9898d1f30baa16599
PavelKozaev <pkozaev221299@gmail.com>
2024-04-01 12:05:04 +0300

    Исправил замечания

--- OnlineShop/OnlineShopWebApp/Models/Registration.cs
+++ OnlineShop/OnlineShopWebApp/Models/Registration.cs
@@ -4,6 +4,6 @@
     {
         public string Login { get; set; }
         public string Password { get; set; }
-        public string RepeatPassword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
