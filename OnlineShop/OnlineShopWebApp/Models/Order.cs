474a0533dc618b3b673ee11840d3a3a8a8b34378
PavelKozaev <pkozaev221299@gmail.com>
2024-04-03 20:58:57 +0300

    Добавил валидацию данных при оформлении заказа

--- OnlineShop/OnlineShopWebApp/Models/Order.cs
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -1,12 +1,31 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Order
     {
         public Guid Id{ get; set; }
         public Cart Cart { get; set; }
+
+        [Required(ErrorMessage = "Укажите полностью ваше имя, фамилию и отчество")]
+        [RegularExpression(@"^[а-яА-ЯёЁ\s\-]+$", ErrorMessage = "ФИО может содержать только буквы русского алфавита, пробелы и дефисы")]
+        [Display(Name = "ФИО")]
         public string Name { get; set; }
-        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Укажите E-mail")]
+        [EmailAddress(ErrorMessage = "Некорректный E-mail адрес")]
+        [Display(Name = "Email")]
+        public string EMail { get; set; }
+
+        [Required(ErrorMessage = "Укажите Телефон")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Номер телефона должен содержать 11 цифр")]
+        [Phone(ErrorMessage = "Некорректный номер телефона")]
+        [Display(Name = "Телефон")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Укажите Адрес")]
+        [RegularExpression(@"^[а-яА-ЯёЁ0-9s.,-/]+$", ErrorMessage = "Адрес может содержать буквы, цифры, пробелы и символы . , - /")]
+        [Display(Name = "Адрес")]
         public string Address { get; set; }
     }
 }
