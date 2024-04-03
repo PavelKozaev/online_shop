28cf03411d0e04250d8e02b551d0fda7cb518aec
PavelKozaev <pkozaev221299@gmail.com>
2024-04-03 20:03:23 +0300

    Добавил валидацию данных при авторизации и регистрации

--- OnlineShop/OnlineShopWebApp/Models/Authorization.cs
+++ OnlineShop/OnlineShopWebApp/Models/Authorization.cs
@@ -1,8 +1,14 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Authorization
     {
+        [Required(ErrorMessage = "Введите email")]
+        [EmailAddress(ErrorMessage = "Введите валидный email")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
         public string Password { get; set; }
         public bool IsRememberMe { get; set; }
     }
--- OnlineShop/OnlineShopWebApp/Models/Registration.cs
+++ OnlineShop/OnlineShopWebApp/Models/Registration.cs
@@ -1,9 +1,18 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Registration
     {
+        [Required(ErrorMessage = "Введите email")]
+        [EmailAddress(ErrorMessage = "Введите валидный email")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Повторите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
     }
 }
