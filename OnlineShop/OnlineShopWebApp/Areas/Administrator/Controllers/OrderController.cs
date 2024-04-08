8aa7bc71223c4bfcce1d2422e512a5a7d5225ad7
PavelKozaev <pkozaev221299@gmail.com>
2024-04-08 19:17:29 +0300

    Разбил AdministratorController на 4 контроллера

---
+++ OnlineShop/OnlineShopWebApp/Areas/Administrator/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using OnlineShopWebApp.Repositories.Interfaces;
+
+namespace OnlineShopWebApp.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class OrderController : Controller
+    {
+        private readonly IOrdersRepository ordersRepository;
+
+        public OrderController(IOrdersRepository ordersRepository)
+        {
+            this.ordersRepository = ordersRepository;
+        }
+        public IActionResult Index()
+        {
+            var orders = ordersRepository.GetAll();
+            return View(orders);
+        }        
+
+        public IActionResult Details(Guid orderId)
+        {
+            var order = ordersRepository.TryGetById(orderId);
+            return View(order);
+        }
+
+        public IActionResult UpdateOrderStatus(Guid orderId, OrderStatus status)
+        {
+            ordersRepository.UpdateStatus(orderId, status);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Areas/Administrator/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using OnlineShopWebApp.Repositories.Interfaces;
+
+namespace OnlineShopWebApp.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class ProductController : Controller
+    {
+        private readonly IProductsRepository productsRepository;
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+        public IActionResult Index()
+        {
+            var products = productsRepository.GetAll();
+
+            if (products == null)
+            {
+                return NotFound("Книги не найдены");
+            }
+
+            return View(products);
+        }    
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productsRepository.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var product = productsRepository.TryGetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Guid id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    productsRepository.Edit(product);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                }
+            }
+            return View(product);
+        }
+
+        public IActionResult Remove(Guid id)
+        {
+            var product = productsRepository.TryGetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("RemoveConfirmed")]
+        public IActionResult RemoveConfirmed(Guid id)
+        {
+            productsRepository.Remove(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
