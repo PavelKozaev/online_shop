162fd0aa1ac3ca16b3c9217b3eadd03fb0fb6c74
PavelKozaev <pkozaev221299@gmail.com>
2024-04-22 20:14:25 +0300

    Баг

---
+++ OnlineShop/OnlineShop.Db/Repositories/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShop.Db.Models;
+
+namespace OnlineShopWebApp.Repositories.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        void Add(Order order);
+        List<Order> GetAll();
+        Order TryGetById(Guid id);
+        void UpdateStatus(Guid orderId, OrderStatus newStatus);
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/UserDeliveryInfoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class UserDeliveryInfoViewModel
+    {
+        [Required(ErrorMessage = "Укажите полностью ваше имя, фамилию и отчество")]
+        [RegularExpression(@"^[а-яА-ЯёЁ\s\-]+$", ErrorMessage = "ФИО может содержать только буквы русского алфавита, пробелы и дефисы")]
+        [Display(Name = "ФИО")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите E-mail")]
+        [EmailAddress(ErrorMessage = "Некорректный E-mail адрес")]
+        [Display(Name = "Email")]
+        public string EMail { get; set; }
+
+        [Required(ErrorMessage = "Укажите Телефон")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Номер телефона должен содержать 11 цифр")]
+        [Phone(ErrorMessage = "Некорректный номер телефона")]
+        [Display(Name = "Телефон")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Укажите Адрес")]
+        [RegularExpression(@"^[а-яА-ЯёЁ0-9s.,-/]+$", ErrorMessage = "Адрес может содержать буквы, цифры, пробелы и символы . , - /")]
+        [Display(Name = "Адрес")]
+        public string Address { get; set; }
+    }
+}
