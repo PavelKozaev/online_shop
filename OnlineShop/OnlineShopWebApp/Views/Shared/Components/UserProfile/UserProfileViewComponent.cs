8399ddfee51c19a84a0ab23a049639fe0b087fdd
PavelKozaev <pkozaev221299@gmail.com>
2024-05-27 15:58:59 +0300

    Вынес отображение аватарки в компоненты представлений

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/UserProfile/UserProfileViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db.Models;
+
+namespace OnlineShopWebApp.Views.Shared.Components.UserProfile
+{
+    public class UserProfileViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> userManager;
+
+        public UserProfileViewComponent(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await userManager.FindByNameAsync(User.Identity.Name);
+            var avatarPath = user.Avatar;
+            return View("UserProfile", avatarPath);
+        }
+    }
+}
